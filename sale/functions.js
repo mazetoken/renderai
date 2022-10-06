18b50a7ff23ea223fd5eda4bdf2c4d3f5beadf6a
Maze Token <68662262+mazetoken@users.noreply.github.com>
2022-10-06 20:26:45 +0200

    Update sale index.html

--- sale/functions.js
+++ sale/functions.js
@@ -1,14 +1,14 @@
 function wallet() {
     var content = "";
-        var event = contractRenderaiNFT.methods.balanceOf(zombieMaster).call({ from: zombieMaster })
-        .then(function (result) {
+    var event = contractRenderaiNFT.methods.balanceOf(zombieMaster).call({ from: zombieMaster })
+      .then(function (result) {
     balance = result;
     for(var i = 0; i < balance; i++){
     var event = contractRenderaiNFT.methods.tokenOfOwnerByIndex(zombieMaster, i).call({ from: zombieMaster })
         .then(function (result) {
     var event = contractRenderaiNFT.methods.tokenURI(Number(result)).call()
         .then(function (result1) {
-    content += "<img src=https://bafybeicii4k6idvzbj6fjcw73vkhdaqddclrelgvqmvdgdqf2mjjexzu4m.ipfs.nftstorage.link/"+result+".png width=256 height=256>" + " Id: " + result;
+    content += "<img src=https://bafybeids4udwtkatf5xvspez3ynkbs5vhgljaczfx3643yndfjew6qe7pq.ipfs.nftstorage.link/"+result+".png width=256 height=256>" + " Id: " + result;
     $("#lang1").html(content);
     });
     });
@@ -26,7 +26,7 @@ function sendNFT() {
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent!: ";
-            //alert("Done!");
+            alert("Done!");
     content += JSON.stringify(receipt.transactionHash);
     $("#lang2").html(content);
         });;
@@ -44,7 +44,7 @@ function wallet2() {
         .then(function (result) {
     var event = contractRenderaiNFT.methods.tokenURI(Number(result)).call()
         .then(function (result1) {
-    content += "<img src=https://bafybeicii4k6idvzbj6fjcw73vkhdaqddclrelgvqmvdgdqf2mjjexzu4m.ipfs.nftstorage.link/"+result+".png width=256 height=256>" + " Id: " + result;
+    content += "<img src=https://bafybeids4udwtkatf5xvspez3ynkbs5vhgljaczfx3643yndfjew6qe7pq.ipfs.nftstorage.link/"+result+".png width=256 height=256>" + " Id: " + result;
     $("#lang3").html(content);
     });
     });
@@ -61,8 +61,38 @@ function buyNFT() {
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent!: ";
-            //alert("Done! You bought it.");
+            alert("Done! You bought it.");
     content += JSON.stringify(receipt.transactionHash);
     $("#lang4").html(content);
         });;
 };
+
+function withdraw() {
+    var tokenId3 = $("#tokenId3").val();
+    var content = "Sending transaction from: ";
+    content += zombieMaster;
+    $("#lang5").html(content);
+    var event = contractRenderaiNFTSale.methods.withdrawNft(tokenId3).send({ from: zombieMaster })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "Transaction sent!: ";
+            alert("Done! You bought it.");
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang5").html(content);
+        });;
+  };
+
+  function transferV() {
+    var address2 = $("#address2").val();
+    var content = "Sending transaction from: ";
+    content += zombieMaster;
+    $("#lang6").html(content);
+    var event = contractRenderaiNFTSale.methods.transferValue(address2).send({ from: zombieMaster })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "Transaction sent!: ";
+            alert("Done! You bought it.");
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang6").html(content);
+        });;
+  };
